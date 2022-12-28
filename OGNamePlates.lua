5c0246edacd7cb1357d945ea66364b38dafbd691
AlexPed <120525650+AlexPed@users.noreply.github.com>
2022-12-28 22:41:14 +0100

    Purple

--- OGNamePlates.lua
+++ OGNamePlates.lua
@@ -4,32 +4,29 @@ local function colorBorder(unitFrame, r, g, b)
     unitFrame.healthBar.border:SetVertexColor(r, g, b)
 end
 
+local function mainColor(unitFrame)
+    if UnitIsUnit(unitFrame.unit, "target") then
+        colorBorder(unitFrame, 1, 0, 1)
+    else
+        colorBorder(unitFrame, 0, 0, 1)
+    end
+end
+
 local function onEvent(self, event, unit)
     local namePlate = C_NamePlate.GetNamePlateForUnit(unit)
 
     if namePlate.UnitFrame and event == 'NAME_PLATE_UNIT_ADDED' or event == 'NAME_PLATE_UNIT_REMOVED' then
         namePlate.UnitFrame.allowBorderColor = true
 
-        if UnitIsUnit(unit, "target") then
-            colorBorder(namePlate.UnitFrame, 1, 0, 0)
-        else
-            colorBorder(namePlate.UnitFrame, 0, 0, 1)
-        end
-
-
+        mainColor(namePlate.UnitFrame)
     else
         namePlate.UnitFrame.allowBorderColor = nil
     end
-
 end
 
 local function updateHealthBorder(unitFrame)
     if unitFrame.allowBorderColor then
-        if UnitIsUnit(unitFrame.unit, "target") then
-            colorBorder(unitFrame, 1, 0, 0)
-        else
-            colorBorder(unitFrame, 0, 0, 1)
-        end
+        mainColor(unitFrame)
     end
 end
 
