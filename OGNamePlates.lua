0eb6a978af95e1830963ab0a28fa14bd7e16c510
AlexPed <120525650+AlexPed@users.noreply.github.com>
2022-12-28 22:23:50 +0100

    Small fixes :+1:

--- OGNamePlates.lua
+++ OGNamePlates.lua
@@ -1,39 +1,49 @@
 local addonName = ...
 
-local function colorBorder(unitFrame)
-    unitFrame.healthBar.border:SetVertexColor(0, 0, 1)
-end
-
-local function log(message)
-    if DLAPI then
-        DLAPI.DebugLog(addonName, message)
-    end
+local function colorBorder(unitFrame, r, g, b)
+    unitFrame.healthBar.border:SetVertexColor(r, g, b)
 end
 
 local function onEvent(self, event, unit)
     local namePlate = C_NamePlate.GetNamePlateForUnit(unit)
 
-    if namePlate.UnitFrame then
-        if event == 'NAME_PLATE_UNIT_ADDED' or event == 'NAME_PLATE_UNIT_REMOVED' then
-            namePlate.UnitFrame.allowBorderColor = true
-            colorBorder(namePlate.UnitFrame)
+    if namePlate.UnitFrame and event == 'NAME_PLATE_UNIT_ADDED' or event == 'NAME_PLATE_UNIT_REMOVED' then
+        namePlate.UnitFrame.allowBorderColor = true
+
+        if UnitIsUnit(unit, "target") then
+            colorBorder(namePlate.UnitFrame, 1, 0, 0)
         else
-            namePlate.UnitFrame.allowBorderColor = nil
+            colorBorder(namePlate.UnitFrame, 0, 0, 1)
         end
+
+
+    else
+        namePlate.UnitFrame.allowBorderColor = nil
     end
+
 end
 
 local function updateHealthBorder(unitFrame)
     if unitFrame.allowBorderColor then
-        colorBorder(unitFrame)
+        if UnitIsUnit(unitFrame.unit, "target") then
+            colorBorder(unitFrame, 1, 0, 0)
+        else
+            colorBorder(unitFrame, 0, 0, 1)
+        end
     end
 end
 
-hooksecurefunc('CompactUnitFrame_UpdateHealthBorder', updateHealthBorder)
+local function HookCompactUnitFrame_UpdateHealthBorder()
+    hooksecurefunc('CompactUnitFrame_UpdateHealthBorder', updateHealthBorder)
+end
 
-local frame = CreateFrame('Frame')
-frame:RegisterEvent('NAME_PLATE_UNIT_ADDED')
-frame:RegisterEvent('NAME_PLATE_UNIT_REMOVED')
-frame:SetScript('OnEvent', onEvent)
+local function InitializeFrame()
+    local frame = CreateFrame('Frame')
+    frame:RegisterEvent('NAME_PLATE_UNIT_ADDED')
+    frame:RegisterEvent('NAME_PLATE_UNIT_REMOVED')
+    frame:SetScript('OnEvent', onEvent)
+end
 
+InitializeFrame()
 
+HookCompactUnitFrame_UpdateHealthBorder()
